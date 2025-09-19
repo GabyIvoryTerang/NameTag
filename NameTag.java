b755e25ef52f53bf0ba4827f0adc55f52dd61572
GabyIvoryTerang <gabyivory912@gmail.com>
2025-09-19 21:25:51 +0800

    first commit

---
+++ NameTag.java
@@ -0,0 +1,10 @@
+public class NameTag {
+
+    public static void main(String[] args) {
+        System.out.println( "==============================");
+        System.out.println ("|| Nama : Gaby Ivory Terang ||");
+        System.out.println ("|| Umur : 17 Tahun          ||");
+        System.out.println ("|| Aktif : Mahasiswa        ||");
+        System.out.println("==============================");
+    }
+}
